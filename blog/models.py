20e1aa87ee8ca10610067464b613d3a79a910225
Hairutdin <alhairunbenali05@gmail.com>
2020-07-13 19:08:56 +0300

    add models Tag, Post, Comment; provide site with posts

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,25 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Tag(models.Model):
+    """Модель тегов"""
+    name = models.CharField("Тег", max_length=100)
+    slug = models.SlugField("url", max_length=100)
+
+
+class Post(models.Model):
+    """Модель постов"""
+    title = models.CharField("Заголовок", max_length=100)
+    mini_text = models.TextField("Мини-текст", max_length=100)
+    text = models.TextField("Текст", max_length=1000)
+    created_date = models.DateTimeField("Дата создания", max_length=100)
+    slug = models.SlugField("url", max_length=100)
+    
+
+class Comment(models.Model):
+    """Модель комментариев"""
+    text = models.TextField("Текст", max_length=100)
+    created_date = models.DateTimeField("Дата создания", max_length=100)
+    moderation = models.BooleanField("Модерация")
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Category, Post
+
+
+
+class HomeView(View):
+    """Home page"""
+    def get(self, request):
+        category_list = Category.objects.all()
+        return render(request, "blog/home.html", {"categories": category_list})
+
+    def get_post(self, request):
+        list_of_posts = Post.objects.all()
+        return  render(request, "blog/home.html", {"posts": list_of_posts})
+
+class CategoryView(View):
+    """Вывод статей категории"""
+    def get(self, request, slug):
+        category = Category.objects.get(slug=slug)
+        return render(request, "blog/post_list.html", {"category": category})
+
+    def get_post(self, request):
+        post = Post.objects.get(slug=slug)
+        return render(request, "blog/post_list.html", {"post": post})
