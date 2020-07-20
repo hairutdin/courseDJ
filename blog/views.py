180eb263217028d24425e58d0a41b77dc70cf87e
Hairutdin <alhairunbenali05@gmail.com>
2020-07-20 17:10:08 +0300

    fixed different stuff

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,7 @@ from datetime import datetime
 from django.shortcuts import render
 from django.views.generic.base import View
 
-from .models import Category, Post
-
-
+from .models import Category, Post, Comment
 
 
 class HomeView(View):
@@ -13,14 +11,7 @@ class HomeView(View):
     def get(self, request):
         category_list = Category.objects.all()
         post_list = Post.objects.filter(published_date__lte=datetime.now(), published=True)
-        return render(request, "blog/post_list.html", {"categories": category_list})
-
-
-class CategoryView(View):
-    """Вывод статей категории"""
-    def get(self, request, slug):
-        category = Category.objects.get(slug=slug)
-        return render(request, "blog/post_list.html", {"category": category})
+        return render(request, "blog/post_list.html", {"categories": category_list, "post_list": post_list})
 
 
 class PostDetailView(View):
@@ -28,4 +19,11 @@ class PostDetailView(View):
     def get(self, request, category, slug):
         category_list = Category.objects.all()
         post = Post.objects.get(slug=slug)
-        return render(request, "blog/detail.html", {"categories": category_list, "post": post})
+        return render(request, post.template, {"categories": category_list, "post": post})
+
+
+class CategoryView(View):
+    """Вывод статей категории"""
+    def get(self, request, category_name):
+        category = Category.objects.get(slug=category_name)
+        return render(request, "blog/post_list.html", {"category": category})
